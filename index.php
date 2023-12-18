ada495e5488145696026635c157fc6e0ed0a1885
latthisit <152863025+latthisit@users.noreply.github.com>
2023-12-18 19:39:11 +0700

    new
    new

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <h1>This is PHP <?php echo 'Hello world'; ?></h1>
+        <div>
+            <p>1</p>
+            <?php
+        
+            ?>
+            <p>2</p>
+            <?php
+            
+            ?>
+        </div>
+    </body>
+</html>
