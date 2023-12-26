41559a2566fcd308765c3302a559bb3c322052a4
latthisit <152863025+latthisit@users.noreply.github.com>
2023-12-26 18:31:00 +0700

    new php
    php

--- index.php
+++ index.php
@@ -1,8 +1,33 @@
 <html>
-    <body>
-        <h1>This is PHP <?php echo 'Hello world'; ?></h1>
-        <div>
-            <?php each 'hello ';?>
-        </div>
-    </body>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    
+    
+    <?php $multi_x = 2; ?>
+    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+
+    <table>
+        <tbody>
+          <?php
+          for ($i = 1; $i <= 24; $i++) {
+              $result = $multi_x * $i;
+              echo "<tr>";
+              echo "<td>{$multi_x} x {$i}</td>";
+              echo "<td>{$result}</td>";
+              echo "</tr>";
+          }
+          ?>
+        </tbody>
+    </table>
+    <style>
+        table {
+            width: 150px;
+            height: 265px;
+        }
+    </style>
+
+
+  </body>
 </html>
